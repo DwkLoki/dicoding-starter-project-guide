ac25273030eb84b5cfca9d8027c459532bca7baa
DwkLoki <dwiky.darmawansyah@gmail.com>
2022-06-21 23:04:30 +0700

    selesai testing e2e

--- e2e/Liking_Restaurant.spec.js
+++ e2e/Liking_Restaurant.spec.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-plusplus */
 /* eslint-disable no-undef */
 const assert = require('assert');
 
@@ -30,4 +32,39 @@ Scenario('liking one restaurant', async ({ I }) => {
   const likedRestaurantTitle = await I.grabTextFrom('.restaurant__title');
 
   assert.strictEqual(firstRestaurantTitle, likedRestaurantTitle);
+});
+
+Scenario('searching restaurants', async ({ I }) => {
+  I.see('Tidak ada restaurant untuk ditampilkan', '.restaurant-item__not__found');
+
+  I.amOnPage('/');
+
+  I.waitForElement('.restaurant__title a');
+
+  const titles = [];
+
+  for (let i = 1; i <= 3; i++) {
+    I.click(locate('.restaurant__title a').at(i));
+    I.waitForElement('#likeButton');
+    I.click('#likeButton');
+    titles.push(await I.grabTextFrom('.restaurant__title'));
+    I.amOnPage('/');
+  }
+
+  I.amOnPage('/#/favorite');
+  I.seeElement('#query');
+
+  const searchQuery = titles[1].substring(1, 3);
+  const matchingRestaurants = titles.filter((title) => title.indexOf(searchQuery) !== -1);
+
+  I.fillField('#query', searchQuery);
+  I.pressKey('Enter');
+
+  const visibleLikedRestaurants = await I.grabNumberOfVisibleElements('.restaurant-item');
+  assert.strictEqual(matchingRestaurants.length, visibleLikedRestaurants);
+
+  matchingRestaurants.forEach(async (title, index) => {
+    const visibleTitle = await I.grabTextFrom(locate('.restaurant__title').at(index + 1));
+    assert.strictEqual(title, visibleTitle);
+  });
 });
